106f63032bd603b3bae1d333bf5607c407ec9e12
ahozi <ahozias@gmail.com>
2021-01-01 16:47:58 +0200

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import create from "zustand";
+import { mountStoreDevtool } from 'simple-zustand-devtools';
+import "./App.css";
 
+const POKEMON_URL =
+  "https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json";
+
+const useStore = create((set) => ({
+  filter: "",
+  pokemon: [],
+  setFilter: (filter) =>
+    set((state) => ({
+      ...state,
+      filter,
+    })),
+  setPokemon: (pokemon) =>
+    set((state) => ({
+      ...state,
+      pokemon,
+    })),
+}));
+
+const FilterInput = () => {
+  const filter = useStore((state) => state.filter);
+  const setFilter = useStore((state) => state.setFilter);
+
+  return <input value={filter} onChange={(e) => setFilter(e.target.value)} />;
+};
+const PokemonTable = () => {
+  const pokemon = useStore((state) => state.pokemon);
+  const filter = useStore((state) => state.filter);
+  console.log(pokemon);
+
+  return (
+    <table width="100%">
+      <tbody>
+        {pokemon
+          .filter(({ name: { english } }) =>
+            english.toLowerCase().includes(filter.toLowerCase())
+          )
+          .map(({ id, name: { english }, type }) => (
+            <tr key={id}>
+              <td>{english}</td>
+              <td>{type.join("", "")}</td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
+if (process.env.NODE_ENV === 'development') {
+  mountStoreDevtool('Store', useStore);
+}
 function App() {
+  const filter = useStore((state) => state.filter);
+  const setPokemon = useStore((state) => state.setPokemon);
+
+  useEffect(() => {
+    fetch(POKEMON_URL)
+      .then((res) => res.json())
+      .then((pokemon) => setPokemon(pokemon));
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <FilterInput />
+      </div>
+      <h1>List of Pokemons</h1>
+      <PokemonTable />
     </div>
   );
 }
